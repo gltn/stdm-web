f504ccf16a8db8dc7635eea20f4728175981221f
sam <swanjohi9@gmail.com>
2020-12-19 10:51:57 +0300

    Added summaries and columns code

--- stdm_config/views.py
+++ stdm_config/views.py
@@ -8,15 +8,16 @@ from django.shortcuts import render
 from django.db import connection
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+import json
 # from psycopg2 import connect, sql
 # from psycopg2.extras import RealDictCursor
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_PATH = os.path.join(BASE_DIR, 'config/default_configuration.xml')
+CONFIG_PATH = os.path.join(BASE_DIR, 'config/configuration.stc')
+reader = StdmConfigurationReader(CONFIG_PATH)
+reader.load()
+stdm_config = StdmConfiguration.instance()
 @login_required
-def STDMReader(request):
-	reader = StdmConfigurationReader(CONFIG_PATH)
-	reader.load()
-	stdm_config = StdmConfiguration.instance()
+def STDMReader(request):	
 	profiles_list = []
 	entities = []
 	default_profile = None
@@ -24,16 +25,12 @@ def STDMReader(request):
 	columns = []
 	for profile in stdm_config.profiles.values(): 
 		profiles_list.append(profile.name)
-
-	print('Profile List', profiles_list)
 	if profiles_list:
 		# Get settings details and retrieve default settings
 		if Setting.objects.exists():
 			configs =Setting.objects.all().first()
 			if configs:
-				print(configs.default_profile)
 				if configs.default_profile in profiles_list:
-					print('yes yes')
 					default_profile = configs.default_profile
 				else:
 					default_profile = profiles_list[0]
@@ -42,17 +39,27 @@ def STDMReader(request):
 			configs.save()
 			default_profile = configs.default_profile	
 	# Loop through profiles and get entities
+	spatial_entity = []
 	for profile in stdm_config.profiles.values(): 
 		if profile.name == default_profile:
 			profiler= profile          
 			for entity in profiler.entities.values():
 				if entity.TYPE_INFO == 'ENTITY':
-					entities.append(entity)				
-	
-	#Loop through columns with type geometry and filter records to display on map
-						
-	print(columns)
-	print('default', default_profile)
+					entities.append(entity)
+					for column in entity.columns.values():
+						if column.TYPE_INFO == 'GEOMETRY':
+							spatial_entity.append(entity.name)
+	print('Spatial Entity', spatial_entity)
+	if spatial_entity:
+		spatial_entity_query = spatial_entity[0]
+		with connection.cursor() as cursor:
+			query = "SELECT * FROM {0}".format(spatial_entity_query)
+			cursor.execute(query)
+			columns = [col[0] for col in cursor.description]
+			data = cursor.fetchall()
+			spatial_data = json.dumps(data)
+			print(spatial_data)
+
 	default_entity =  None
 	data = []
 	if entities:
@@ -60,15 +67,27 @@ def STDMReader(request):
 		with connection.cursor() as cursor:
 			query = "SELECT * FROM {0}".format(default_entity.name)
 			cursor.execute(query)
-			col_name = [col[0] for col in cursor.description]
+			columns = [col[0] for col in cursor.description]
 			data = cursor.fetchall()
-	return render(request, 'dashboard/index.html', {'configs': configs,'default_profile':default_profile,'profiles':profiles_list,'entities':entities,'default_entity':default_entity,'data':data})
+			# data = [
+			# 	dict(zip(columns, row))
+			# 	for row in cursor.fetchall()
+			# ]
+			# print(data)
+		summaries = {'name':[],'count':[]}
+		with connection.cursor() as cursor:
+			for en in entities:
+				query = "SELECT * FROM {0}".format(en.name)
+				cursor.execute(query)
+				data = cursor.fetchall()
+				summaries["name"].append(en.short_name)
+				summaries["count"].append(len(data))
+		zipped_summaries = zip(summaries["name"][:4],summaries["count"][:4])
+
+	return render(request, 'dashboard/index.html', {'configs': configs,'default_profile':default_profile,'profiles':profiles_list,'entities':entities,'default_entity':default_entity,'data':data,'summaries':zipped_summaries,'spatial_data':spatial_data})
 
 @csrf_exempt
 def ProfileUpdatingView(request, profile):
-	reader = StdmConfigurationReader(CONFIG_PATH)
-	reader.load()
-	stdm_config = StdmConfiguration.instance() 
 	entities = []
 	for profiles in stdm_config.profiles.values():
 		if profiles.name == profile:
@@ -84,16 +103,18 @@ def ProfileUpdatingView(request, profile):
 	with connection.cursor() as cursor:
 		query = "SELECT * FROM {0}".format(default_entity.name)
 		cursor.execute(query)
-		col_name = [col[0] for col in cursor.description]
+		columns = [col[0] for col in cursor.description]
 		data = cursor.fetchall()
-		print(data)
+		# print(data)
+		# data = [
+		# 	dict(zip(columns, row))
+		# 	for row in cursor.fetchall()
+		# ]
+		# print(data)
 	return render(request,'dashboard/records.html', {'entities':entities,'default_entity':default_entity,'data':data})
 
 @csrf_exempt
-def EntityListingUpdatingView(request, profile):
-	reader = StdmConfigurationReader(CONFIG_PATH)
-	reader.load()
-	stdm_config = StdmConfiguration.instance()      
+def EntityListingUpdatingView(request, profile):  
 	entity_list = []
 	for profiles in stdm_config.profiles.values():
 		if profiles.name == profile: 			
@@ -106,9 +127,6 @@ def EntityListingUpdatingView(request, profile):
 
 @csrf_exempt
 def EntityDetailView(request, entity_name):
-	reader = StdmConfigurationReader(CONFIG_PATH)
-	reader.load()
-	stdm_config = StdmConfiguration.instance()
 	entity_detail = None
 	entities = [] 
 	for profiles in stdm_config.profiles.values():      
@@ -129,9 +147,6 @@ def EntityDetailView(request, entity_name):
 
 @csrf_exempt
 def SummaryUpdatingView(request, profile):
-	reader = StdmConfigurationReader(CONFIG_PATH)
-	reader.load()
-	stdm_config = StdmConfiguration.instance() 
 	entities = []
 	for profiles in stdm_config.profiles.values():
 		if profiles.name == profile:
@@ -142,11 +157,13 @@ def SummaryUpdatingView(request, profile):
 					columns = []
 					for column in entity.columns.values():
 						columns.append(column.name)
-
+	summaries = {'name':[],'count':[]}
 	with connection.cursor() as cursor:
 		for en in entities:
 			query = "SELECT * FROM {0}".format(en.name)
 			cursor.execute(query)
 			data = cursor.fetchall()
-			print('records',en,':',len(list(cursor)))
-	return render(request,'dashboard/summary.html', {'entities':entities})
+			summaries["name"].append(en.short_name)
+			summaries["count"].append(len(data))
+	zipped_summaries = zip(summaries["name"][:4],summaries["count"][:4])
+	return render(request,'dashboard/summary.html', {'entities':entities,'summaries':zipped_summaries})
