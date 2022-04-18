8430e6671bcdc9cbab52ca084ca8914a67394339
wanjohikibui <swanjohi9@gmail.com>
2022-04-18 16:40:52 +0300

    Added exception handling for entities with errors. Added endpoint to load geojson for aone record

--- stdm_config/urls.py
+++ stdm_config/urls.py
@@ -8,10 +8,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [  
     path('api/<profile>', ProfileUpdatingView, name='profile_detail'),
     path('profile/api/<profile>', EntityListingUpdatingView, name='profiles_detail'),
-    path('<profile_name>/entity/<short_name>', EntityDetailView, name='entity_detail'),
+    path('<profile_name>/entity/<entity_short_name>', EntityDetailView, name='entity_detail'),
     path('summary/<profile>', SummaryUpdatingView, name='summary_detail'),
     path('<profile_name>/<entity_short_name>/<id>', EntityRecordViewMore, name='fetchstr'),
     path('tables/<table_name>',get_table_columns, name='db_table_columns'),
     path('mobile/data/sync/', MobileSyncDataView, name='mobile_data_sync'),
+    path('geojson/profile/<profile_name>/entity/<entity_short_name>/<row_id>', sp_unit_geojson, name='get_entity_geojson_by_id')
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
