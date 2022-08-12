88a63e6afb331a1092bb5f3baa5695461ba406ed
sam <swanjohi9@gmail.com>
2022-08-12 12:09:54 +0300

    Fixed issues on repo

--- app/admin.py
+++ app/admin.py
@@ -6,22 +6,25 @@ from django.contrib.admin.models import LogEntry
 class LogEntryAdmin(admin.ModelAdmin):
     list_display = ('content_type', 'user', 'action_time')
 
-
 admin.site.register(LogEntry, LogEntryAdmin)
+
+@admin.register(EntityError)
+class EntityErrorAdmin(admin.ModelAdmin):
+    list_display = ('entity', 'date')
 # Register your models here.
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     pass
 
 
-@admin.register(Setting)
-class SettingAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Setting)
+# class SettingAdmin(admin.ModelAdmin):
+#     pass
 
-    def has_add_permission(self, *args, **kwargs):
-        return not Setting.objects.exists()
+#     def has_add_permission(self, *args, **kwargs):
+#         return not Setting.objects.exists()
 
 
 @admin.register(Configuration)
